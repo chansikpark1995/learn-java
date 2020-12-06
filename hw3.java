5e6a3b3294c5f6641b07ca349f7a6ed16caf2713
chansikpark1995 <75366853+chansikpark1995@users.noreply.github.com>
2020-12-06 13:37:26 +0900

    Add files via upload
    유튜브 혼공자바의 배열 내용이다.
    배열의 기본적인 구조, 배열을 이용한 간단한 for문을 배웠다.
    
    학원의 진도가 빨라서 내용을 이해하기 어려운 점이 많다.
    앞으로 집에서도 예습 복습을 통해 깃허브에 저장할 예정이다.

---
+++ hw3.java
@@ -0,0 +1,108 @@
+package practice;
+
+public class hw3 { 
+	public static void main(String[] args){
+		/*
+		int[] scores = {83,90,87};
+		
+		System.out.println("scores[0]: " +scores[0]);
+		System.out.println("scores[1]: " +scores[1]);
+		System.out.println("scores[2]: " +scores[2]);
+
+		int sum = 0;
+		for(int i=0; i<3; i++)
+		{
+			sum += scores[i];
+			
+		}
+		System.out.println("총합 : "+sum);
+		double avg = (double) sum/3;
+		System.out.println("평균 : "+avg);
+		
+		*/
+		
+		/*
+		int[] scores1;
+		scores1 = new int[] {84,91,88};
+		int sum1 = 0;
+		for(int i=0; i<3; i++)
+		{
+			sum1 +=scores1[i];
+		
+		}
+		System.out.println("총합 : "+sum1);
+		
+		int sum3 = add(new int[] {84,91,88});
+		System.out.println("총합 : "+sum3);
+		
+	}
+		
+		
+		public static int add(int[] scores1){
+			int sum2 = 0;
+			for(int i=0; i<3; i++)
+			{
+				sum2 +=scores1[i];
+			}
+		return sum2;
+		
+		배열 선언의 다른 방법. 위의 예제는 타입 변수[] = {내용물들} 형태로 선언했으나
+		해당 예제는 타입 변수; 변수 = new 타입[]{내용물들} 형태로, 변수 따로 내용물 따로 선언했다.
+		이는 new 가 꼭 붙어야 한다. new[] 의 대괄호 안의 숫자는 공간의 갯수다.
+		배열 공간을 인덱스라고 한다.
+		*/
+		/*
+		int[] arr1 = new int[3];
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("arr1[ "+ i + " ] : " + arr1[i] );
+			
+		}
+		arr1[0] = 10;
+		arr1[1] = 20;
+		arr1[2] = 30;
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("arr1[ "+ i + " ] : " + arr1[i] );
+			
+		}
+		double[] arr2 = new double[3];
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("arr1[ "+ i + " ] : " + arr2[i] );
+			
+		}
+		arr2[0] = 0.1;
+		arr2[1] = 0.3;
+		arr2[2] = 0.5;
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("arr1[ "+ i + " ] : " + arr2[i] );
+			
+		}
+		String[] arr3 = new String[3];
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("arr1[ "+ i + " ] : " + arr3[i] );
+			
+		}
+		
+		arr3[0] = "1월";
+		arr3[1] = "3월";
+		arr3[2] = "5월";
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("arr1[ "+ i + " ] : " + arr3[i] );
+			
+		}
+		
+		배열 생성시 각 변수타입의 초기값이 다름을 확인할 수 있다.
+		int는 0, double은 0.0, String은 null이 저장된다.
+		new 타입[공간갯수]로 선언 후 각 공간에 원하는 변수를 넣고 싶으면
+		변수[공간] = 원하는내용; 형태로 선언하면 된다.
+	
+		*/
+		}
+
+}
+
