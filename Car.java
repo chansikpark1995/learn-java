c4ff8e49c57c323961884d2d0a37a13c3fa12384
chansikpark1995 <75366853+chansikpark1995@users.noreply.github.com>
2020-12-13 19:55:00 +0900

    Setter&Getter 메소드 예습
    접근제한자 내용과 이어지는 세터메소드와 게터메소드를 예습했다.
    코로나가 점점 심해진다. 일일 확진자가 천명씩이나 나왔다.
    학원을 못 가는 상황이라 집에서 공부했다.
    주어진 상황 속에서 하는 만큼 해보자.

--- Car.java
+++ Car.java
@@ -1,11 +1,28 @@
-package practice;
+package sec06.exam06;
 
 public class Car {
-
-	String company = "현대자동차";
-	String model = "그런저";
-	String color = "검정";
 	
-	int maxSpeed = 350;
-	int speed;
+	private int speed;
+	private boolean stop;
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		if(speed<0) {
+			this.speed = 0;
+			return;
+		}
+		else{
+			this.speed = speed; }
+	}
+	
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		if(stop) {
+			speed = 0;
+		}
+	}
 }
--- CarExample.java
+++ CarExample.java
@@ -1,15 +1,19 @@
-package practice;
+package sec06.exam06;
 
 public class CarExample {
 	public static void main(String[] args) {
-		Car myCar = new Car();
-		System.out.println("제작회사 : "+myCar.company);
-		System.out.println("모델명 : "+myCar.model);
-		System.out.println("색깔 : "+myCar.color);
-		System.out.println("최고속도 : "+myCar.maxSpeed);
-		System.out.println("속도 : "+myCar.speed);
 		
-		myCar.speed = 60; // Car 클래스 밖에서 필드 수정.변수명 선언 후 참조변수를 통해 수정
-		System.out.println("속도 : "+myCar.speed);
+		Car myCar = new Car();
+		
+		myCar.setSpeed(60);
+		System.out.println("현재 속도는"+myCar.getSpeed());
+		
+		if(!myCar.isStop()) {
+			
+			myCar.setStop(true);
+			
+		}
+		System.out.println("현재 속도는"+myCar.getSpeed());
 	}
+
 }
